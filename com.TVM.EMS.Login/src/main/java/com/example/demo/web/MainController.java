cb7190d901ab7f61a370b7762a3c20f114b7ed83
Ruthna Kumar S <71136357+RuthnaKumar@users.noreply.github.com>
2021-06-30 13:46:57 +0530

    EMS

---
+++ com.TVM.EMS.Login/src/main/java/com/example/demo/web/MainController.java
@@ -0,0 +1,19 @@
+package com.example.demo.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
